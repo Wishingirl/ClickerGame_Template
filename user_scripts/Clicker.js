a7b8823c4b4574a6ed186d479abdea2e7dd55d7a
Shiggy <65451703+Core-commits@users.noreply.github.com>
2020-07-22 12:49:16 +0200

    Clean up

--- user_scripts/Clicker.js
+++ user_scripts/Clicker.js
@@ -4,12 +4,12 @@ var db = flatfile('/db/Score.db');
 let tool = new Tool("Credit Card")
 tool.model = 136999
 Game.on("playerJoin", (player) => {
-   player.on("initialSpawn", () => {
-       player.equipTool(tool)
-   })
+    player.on("initialSpawn", () => {
+        player.equipTool(tool)
+    })
 })
 tool.on("activated", (p) => {
-  console.log(p.username + " HAS TAKEN MONERS");
-  p.setScore(p.score+1)
-  db.put(p.userId, p.score)
+    console.log(p.username + " HAS TAKEN MONERS");
+    p.setScore(p.score + 1)
+    db.put(p.userId, p.score)
 })
