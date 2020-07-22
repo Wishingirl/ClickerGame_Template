2f8a4e38b0d721b396fa62edef3caf7905fe9179
Shiggy <65451703+Core-commits@users.noreply.github.com>
2020-07-22 12:50:19 +0200

    Update start.js

--- start.js
+++ start.js
@@ -1,19 +1,20 @@
 const nh = require('node-hill')
 
 nh.startServer({
-    gameId: 23891,// Your game id here
-
+    gameId: 23891, // Your game id here
+    
     port: 42480, // Your port id here (default is 42480)
-
+    
     local: false, // Whether or not your server is local
-
+    
     // map: './maps/mygame.brk', - Your .brk file location here
     
     scripts: './user_scripts', // Your .js files location
-
-    sandbox: {flatfile: require("flat-file-db")
-	     
-}, // Your npm modules you want to add to the VM 
-
+    
+    sandbox: {
+        flatfile: require("flat-file-db")
+        
+    }, // Your npm modules you want to add to the VM 
+    
     // For more help: https://meta_data.gitlab.io/node-hill/interfaces/gamesettings.html
 })
